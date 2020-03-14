dea489e5687beb30e9358538bc561b4c061d1910
luminousbeam <jocelyn.jeriah@gmail.com>
2020-03-14 19:10:01 -0400

    completed Resistor-Color exercise

---
+++ resistor-color/resistor-color.js
@@ -0,0 +1,7 @@
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
+
+export const colorCode = (color) => {
+  let index = COLORS.indexOf(color.toLowerCase())
+  return index
+}
+
