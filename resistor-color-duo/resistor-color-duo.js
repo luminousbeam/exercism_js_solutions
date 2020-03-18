c6ccfddf9bebc031a33a438d357a463de9743b51
luminousbeam <jocelyn.jeriah@gmail.com>
2020-03-18 18:39:14 -0400

    completed Resistor Duo exercise

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,4 @@
+export const COLORS = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"]; 
+
+export const decodedValue = colorPicks => +`${COLORS.indexOf(colorPicks[0])}${COLORS.indexOf(colorPicks[1])}`;
+
